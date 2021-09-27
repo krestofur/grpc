e40f9f5ddaab552b6c30a428bea78404c5ea50a9
Christopher Chan <chrisbchan@google.com>
2021-09-27 12:32:56 -0700

    update

--- test/core/tsi/crl_ssl_transport_security_test.cc
+++ test/core/tsi/crl_ssl_transport_security_test.cc
@@ -195,7 +195,8 @@ class CrlSslTransportSecurityTest : public ::testing::Test {
   CrlSslTransportSecurityTest()
       : vtable_({.setup_handshakers = ssl_test_setup_handshakers,
                  .check_handshaker_peers = ssl_test_check_handshaker_peers,
-                 .destruct = &ssl_test_destruct}) {}
+                 .destruct = &CrlSslTransportSecurityTest::ssl_test_destruct}) {
+  }
   void SetUp() override {
     fixture_ = ssl_tsi_test_fixture_create();
     ssl_fixture_ = reinterpret_cast<ssl_tsi_test_fixture*>(fixture_);
