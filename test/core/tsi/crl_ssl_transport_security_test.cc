4bdfc1c323e4bf3e2020bb4f6d4b33d2f969843f
Christopher Chan <chrisbchan@google.com>
2021-09-27 12:36:49 -0700

    update

--- test/core/tsi/crl_ssl_transport_security_test.cc
+++ test/core/tsi/crl_ssl_transport_security_test.cc
@@ -71,107 +71,6 @@ typedef struct ssl_tsi_test_fixture {
   tsi_ssl_client_handshaker_factory* client_handshaker_factory;
 } ssl_tsi_test_fixture;
 
-static void ssl_test_setup_handshakers(tsi_test_fixture* fixture) {
-  ssl_tsi_test_fixture* ssl_fixture =
-      reinterpret_cast<ssl_tsi_test_fixture*>(fixture);
-  GPR_ASSERT(ssl_fixture != nullptr);
-  GPR_ASSERT(ssl_fixture->key_cert_lib != nullptr);
-  ssl_key_cert_lib* key_cert_lib = ssl_fixture->key_cert_lib;
-  /* Create client handshaker factory. */
-
-  tsi_ssl_client_handshaker_options client_options;
-  client_options.pem_root_certs = key_cert_lib->root_cert;
-  if (key_cert_lib->use_revoked_client_cert) {
-    client_options.pem_key_cert_pair = key_cert_lib->revoked_pem_key_cert_pairs;
-  } else {
-    client_options.pem_key_cert_pair = key_cert_lib->valid_pem_key_cert_pairs;
-  }
-  client_options.crl_directory = key_cert_lib->crl_directory;
-
-  client_options.root_store = key_cert_lib->root_store;
-  client_options.min_tls_version = test_tls_version;
-  client_options.max_tls_version = test_tls_version;
-  GPR_ASSERT(tsi_create_ssl_client_handshaker_factory_with_options(
-                 &client_options, &ssl_fixture->client_handshaker_factory) ==
-             TSI_OK);
-  /* Create server handshaker factory. */
-  tsi_ssl_server_handshaker_options server_options;
-
-  if (key_cert_lib->use_revoked_server_cert) {
-    server_options.pem_key_cert_pairs =
-        key_cert_lib->revoked_pem_key_cert_pairs;
-    server_options.num_key_cert_pairs =
-        key_cert_lib->revoked_num_key_cert_pairs;
-  } else {
-    server_options.pem_key_cert_pairs = key_cert_lib->valid_pem_key_cert_pairs;
-    server_options.num_key_cert_pairs = key_cert_lib->valid_num_key_cert_pairs;
-  }
-
-  server_options.pem_client_root_certs = key_cert_lib->root_cert;
-  server_options.crl_directory = key_cert_lib->crl_directory;
-
-  server_options.client_certificate_request =
-      TSI_REQUEST_AND_REQUIRE_CLIENT_CERTIFICATE_AND_VERIFY;
-  server_options.session_ticket_key = ssl_fixture->session_ticket_key;
-  server_options.session_ticket_key_size = ssl_fixture->session_ticket_key_size;
-  server_options.min_tls_version = test_tls_version;
-  server_options.max_tls_version = test_tls_version;
-  GPR_ASSERT(tsi_create_ssl_server_handshaker_factory_with_options(
-                 &server_options, &ssl_fixture->server_handshaker_factory) ==
-             TSI_OK);
-  /* Create server and client handshakers. */
-  GPR_ASSERT(tsi_ssl_client_handshaker_factory_create_handshaker(
-                 ssl_fixture->client_handshaker_factory,
-                 ssl_fixture->server_name_indication,
-                 &ssl_fixture->base.client_handshaker) == TSI_OK);
-  GPR_ASSERT(tsi_ssl_server_handshaker_factory_create_handshaker(
-                 ssl_fixture->server_handshaker_factory,
-                 &ssl_fixture->base.server_handshaker) == TSI_OK);
-}
-
-static void ssl_test_check_handshaker_peers(tsi_test_fixture* fixture) {
-  ssl_tsi_test_fixture* ssl_fixture =
-      reinterpret_cast<ssl_tsi_test_fixture*>(fixture);
-  GPR_ASSERT(ssl_fixture != nullptr);
-  GPR_ASSERT(ssl_fixture->key_cert_lib != nullptr);
-  ssl_key_cert_lib* key_cert_lib = ssl_fixture->key_cert_lib;
-  tsi_peer peer;
-
-  // In TLS 1.3, the client-side handshake succeeds even if the client sends a
-  // revoked certificate. In such a case, the server would fail the TLS
-  // handshake and send an alert to the client as the first application data
-  // message. In TLS 1.2, the client-side handshake will fail if the client
-  // sends a revoked certificate.
-  //
-  // For OpenSSL versions < 1.1, TLS 1.3 is not supported, so the client-side
-  // handshake should succeed precisely when the server-side handshake
-  // succeeds.
-  bool expect_server_success = !(key_cert_lib->use_revoked_server_cert ||
-                                 key_cert_lib->use_revoked_client_cert);
-#if OPENSSL_VERSION_NUMBER >= 0x10100000
-  bool expect_client_success = test_tls_version == tsi_tls_version::TSI_TLS1_2
-                                   ? expect_server_success
-                                   : !(key_cert_lib->use_revoked_server_cert);
-#else
-  bool expect_client_success = expect_server_success;
-#endif
-
-  if (expect_client_success) {
-    GPR_ASSERT(tsi_handshaker_result_extract_peer(
-                   ssl_fixture->base.client_result, &peer) == TSI_OK);
-    tsi_peer_destruct(&peer);
-  } else {
-    GPR_ASSERT(ssl_fixture->base.client_result == nullptr);
-  }
-  if (expect_server_success) {
-    GPR_ASSERT(tsi_handshaker_result_extract_peer(
-                   ssl_fixture->base.server_result, &peer) == TSI_OK);
-    tsi_peer_destruct(&peer);
-  } else {
-    GPR_ASSERT(ssl_fixture->base.server_result == nullptr);
-  }
-}
-
 static void ssl_test_pem_key_cert_pair_destroy(tsi_ssl_pem_key_cert_pair kp) {
   gpr_free(const_cast<char*>(kp.private_key));
   gpr_free(const_cast<char*>(kp.cert_chain));
@@ -222,12 +121,119 @@ class CrlSslTransportSecurityTest : public ::testing::Test {
         ssl_fixture->client_handshaker_factory);
   }
 
+  void ssl_test_setup_handshakers(tsi_test_fixture* fixture) {
+    ssl_tsi_test_fixture* ssl_fixture =
+        reinterpret_cast<ssl_tsi_test_fixture*>(fixture);
+    GPR_ASSERT(ssl_fixture != nullptr);
+    GPR_ASSERT(ssl_fixture->key_cert_lib != nullptr);
+    ssl_key_cert_lib* key_cert_lib = ssl_fixture->key_cert_lib;
+    /* Create client handshaker factory. */
+
+    tsi_ssl_client_handshaker_options client_options;
+    client_options.pem_root_certs = key_cert_lib->root_cert;
+    if (key_cert_lib->use_revoked_client_cert) {
+      client_options.pem_key_cert_pair =
+          key_cert_lib->revoked_pem_key_cert_pairs;
+    } else {
+      client_options.pem_key_cert_pair = key_cert_lib->valid_pem_key_cert_pairs;
+    }
+    client_options.crl_directory = key_cert_lib->crl_directory;
+
+    client_options.root_store = key_cert_lib->root_store;
+    client_options.min_tls_version = test_tls_version;
+    client_options.max_tls_version = test_tls_version;
+    GPR_ASSERT(tsi_create_ssl_client_handshaker_factory_with_options(
+                   &client_options, &ssl_fixture->client_handshaker_factory) ==
+               TSI_OK);
+    /* Create server handshaker factory. */
+    tsi_ssl_server_handshaker_options server_options;
+
+    if (key_cert_lib->use_revoked_server_cert) {
+      server_options.pem_key_cert_pairs =
+          key_cert_lib->revoked_pem_key_cert_pairs;
+      server_options.num_key_cert_pairs =
+          key_cert_lib->revoked_num_key_cert_pairs;
+    } else {
+      server_options.pem_key_cert_pairs =
+          key_cert_lib->valid_pem_key_cert_pairs;
+      server_options.num_key_cert_pairs =
+          key_cert_lib->valid_num_key_cert_pairs;
+    }
+
+    server_options.pem_client_root_certs = key_cert_lib->root_cert;
+    server_options.crl_directory = key_cert_lib->crl_directory;
+
+    server_options.client_certificate_request =
+        TSI_REQUEST_AND_REQUIRE_CLIENT_CERTIFICATE_AND_VERIFY;
+    server_options.session_ticket_key = ssl_fixture->session_ticket_key;
+    server_options.session_ticket_key_size =
+        ssl_fixture->session_ticket_key_size;
+    server_options.min_tls_version = test_tls_version;
+    server_options.max_tls_version = test_tls_version;
+    GPR_ASSERT(tsi_create_ssl_server_handshaker_factory_with_options(
+                   &server_options, &ssl_fixture->server_handshaker_factory) ==
+               TSI_OK);
+    /* Create server and client handshakers. */
+    GPR_ASSERT(tsi_ssl_client_handshaker_factory_create_handshaker(
+                   ssl_fixture->client_handshaker_factory,
+                   ssl_fixture->server_name_indication,
+                   &ssl_fixture->base.client_handshaker) == TSI_OK);
+    GPR_ASSERT(tsi_ssl_server_handshaker_factory_create_handshaker(
+                   ssl_fixture->server_handshaker_factory,
+                   &ssl_fixture->base.server_handshaker) == TSI_OK);
+  }
+
+  void ssl_test_check_handshaker_peers(tsi_test_fixture* fixture) {
+    ssl_tsi_test_fixture* ssl_fixture =
+        reinterpret_cast<ssl_tsi_test_fixture*>(fixture);
+    GPR_ASSERT(ssl_fixture != nullptr);
+    GPR_ASSERT(ssl_fixture->key_cert_lib != nullptr);
+    ssl_key_cert_lib* key_cert_lib = ssl_fixture->key_cert_lib;
+    tsi_peer peer;
+
+    // In TLS 1.3, the client-side handshake succeeds even if the client sends a
+    // revoked certificate. In such a case, the server would fail the TLS
+    // handshake and send an alert to the client as the first application data
+    // message. In TLS 1.2, the client-side handshake will fail if the client
+    // sends a revoked certificate.
+    //
+    // For OpenSSL versions < 1.1, TLS 1.3 is not supported, so the client-side
+    // handshake should succeed precisely when the server-side handshake
+    // succeeds.
+    bool expect_server_success = !(key_cert_lib->use_revoked_server_cert ||
+                                   key_cert_lib->use_revoked_client_cert);
+#if OPENSSL_VERSION_NUMBER >= 0x10100000
+    bool expect_client_success = test_tls_version == tsi_tls_version::TSI_TLS1_2
+                                     ? expect_server_success
+                                     : !(key_cert_lib->use_revoked_server_cert);
+#else
+    bool expect_client_success = expect_server_success;
+#endif
+
+    if (expect_client_success) {
+      GPR_ASSERT(tsi_handshaker_result_extract_peer(
+                     ssl_fixture->base.client_result, &peer) == TSI_OK);
+      tsi_peer_destruct(&peer);
+    } else {
+      GPR_ASSERT(ssl_fixture->base.client_result == nullptr);
+    }
+    if (expect_server_success) {
+      GPR_ASSERT(tsi_handshaker_result_extract_peer(
+                     ssl_fixture->base.server_result, &peer) == TSI_OK);
+      tsi_peer_destruct(&peer);
+    } else {
+      GPR_ASSERT(ssl_fixture->base.server_result == nullptr);
+    }
+  }
+
  protected:
   CrlSslTransportSecurityTest()
-      : vtable_({.setup_handshakers = ssl_test_setup_handshakers,
-                 .check_handshaker_peers = ssl_test_check_handshaker_peers,
-                 .destruct = &CrlSslTransportSecurityTest::ssl_test_destruct}) {
-  }
+      : vtable_(
+            {.setup_handshakers =
+                 &CrlSslTransportSecurityTest::ssl_test_setup_handshakers,
+             .check_handshaker_peers =
+                 &CrlSslTransportSecurityTest::ssl_test_check_handshaker_peers,
+             .destruct = &CrlSslTransportSecurityTest::ssl_test_destruct}) {}
   void SetUp() override {
     fixture_ = ssl_tsi_test_fixture_create();
     ssl_fixture_ = reinterpret_cast<ssl_tsi_test_fixture*>(fixture_);
