fa2237a17f7f66ed2afbb080a7883314a7ba3e5e
Christopher Chan <chrisbchan@google.com>
2021-10-05 16:22:37 -0700

    update

--- test/core/tsi/crl_ssl_transport_security_test.cc
+++ test/core/tsi/crl_ssl_transport_security_test.cc
@@ -261,7 +261,7 @@ TEST_F(CrlSslTransportSecurityTest,
   SslTestFixture* fixture = new SslTestFixture(true, false, &kVtable);
   tsi_test_fixture* base = reinterpret_cast<tsi_test_fixture*>(fixture);
   tsi_test_do_handshake(base);
-  tsi_test_fixture_destroy(base);
+  // tsi_test_fixture_destroy(base);
 }
 TEST_F(CrlSslTransportSecurityTest,
        ssl_tsi_test_do_handshake_with_revoked_client_cert) {
@@ -276,7 +276,7 @@ TEST_F(CrlSslTransportSecurityTest,
   SslTestFixture* fixture = new SslTestFixture(false, false, &kVtable);
   tsi_test_fixture* base = reinterpret_cast<tsi_test_fixture*>(fixture);
   tsi_test_do_handshake(base);
-  // tsi_test_fixture_destroy(base);
+  tsi_test_fixture_destroy(base);
 }
 
 int main(int argc, char** argv) {
