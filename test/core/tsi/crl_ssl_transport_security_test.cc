cd8ee2615d8d62a47957c936f521ce5db0000d0b
Christopher Chan <chrisbchan@google.com>
2021-10-07 21:27:22 -0700

    update

--- test/core/tsi/crl_ssl_transport_security_test.cc
+++ test/core/tsi/crl_ssl_transport_security_test.cc
@@ -272,6 +272,21 @@ INSTANTIATE_TEST_SUITE_P(TLSVersionsTest, CrlSslTransportSecurityTest,
 
 int main(int argc, char** argv) {
   grpc::testing::TestEnvironment env(argc, argv);
-  ::testing::InitGoogleTest(&argc, argv);
+  grpc_init();
+
+  // const size_t number_tls_versions = 2;
+  // const tsi_tls_version tls_versions[] = {tsi_tls_version::TSI_TLS1_2,
+  //                                         tsi_tls_version::TSI_TLS1_3};
+  // for (size_t i = 0; i < number_tls_versions; i++) {
+  //   // Set the TLS version to be used in the tests.
+  //   test_tls_version = tls_versions[i];
+  //   // Run all the tests using that TLS version for both the client and
+  //   // server.
+  //   int test_result = RUN_ALL_TESTS();
+  //   if (test_result != 0) {
+  //     return test_result;
+  //   };
+  // }
+  grpc_shutdown();
   return RUN_ALL_TESTS();
 }
