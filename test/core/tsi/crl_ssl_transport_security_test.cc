5fd43d8602101aa8891ef0d13098eaad8f1c5941
Christopher Chan <chrisbchan@google.com>
2021-11-03 14:52:54 -0700

    update

--- test/core/tsi/crl_ssl_transport_security_test.cc
+++ test/core/tsi/crl_ssl_transport_security_test.cc
@@ -229,7 +229,8 @@ class CrlSslTransportSecurityTest
 
     static void Destruct(tsi_test_fixture* fixture) {
       auto* self = reinterpret_cast<SslTsiTestFixture*>(fixture);
-      self->~SslTsiTestFixture();
+      // self->~SslTsiTestFixture();
+      delete self;
     }
 
     static char* LoadFile(absl::string_view file_path) {
